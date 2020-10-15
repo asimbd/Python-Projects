30fac3d5268aef9f56c7c44f59075c7e93f940b7
asimbd <72205854+asimbd@users.noreply.github.com>
2020-10-15 22:38:18 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+command = input("Enter a Type: ")
+
+if command == "+":
+    number1 = int(input("Enter First Number: "))
+    number2 = int(input("Enter Second Number: "))
+    print("Total:",number1, "+" ,number2, "=", number1+number2 )
